8117baa58a6ab7955b763c8ad6bc237981c40c9c
carl <jerritt.aquino@gmail.com>
2020-04-02 21:02:08 -0400

    METHOD: mark square on grid

--- bin/engine.rb
+++ bin/engine.rb
@@ -1,9 +1,28 @@
 class Engine
-  def initialize
-    @grid = Grid.new(5,5)
+  def play
+    get_starting_info
   end
 
-  def play
+  def get_starting_info
+    # returns [width, height, starting_markers_array]
+    puts "Welcome to Conway's Game of Life!"
+    puts "Please enter the width:"
+    width = gets.chomp.to_i
+    puts "Please enter the height:"
+    height = gets.chomp.to_i
+
+    starting_markers = select_starting_markers(width, height)
+
+    [width, height, starting_markers]
+  end
+
+  def select_starting_markers(width, height)
+    puts "Select the cells you want to start alive"
+    puts "(comma separated with no spaces)"
+    
 
   end
 end
+
+
+Engine.new.play
--- lib/grid.rb
+++ lib/grid.rb
@@ -2,7 +2,7 @@ require_relative 'cell'
 
 class Grid
   def initialize(width, height)
-    @grid = [[Cell.new]*width]*height
+    @grid = create_grid(width, height)
   end
 
   def print_grid
@@ -13,4 +13,26 @@ class Grid
       print "\n"
     end
   end
+
+  def mark_square(i, j)
+    @grid[j][i].live
+  end
+
+  def create_grid(width, height)
+    row = []
+    grid = []
+    height.times do |_|
+      width.times { |_| row << Cell.new}
+      grid << row
+      row = []
+    end
+
+    grid
+  end
 end
+
+a = Grid.new(3,3)
+a.print_grid
+puts '___'
+a.mark_square(0,1)
+a.print_grid
