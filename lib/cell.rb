6beae62a94e51e0e9c8b360a2f5a4fabdc65c497
carl <jerritt.aquino@gmail.com>
2020-04-02 20:20:07 -0400

    create engine cell and grid classes

---
+++ lib/cell.rb
@@ -0,0 +1,18 @@
+class Cell
+  STATES = [:dead, :alive]
+  STARTING_STATE = STATES[0]
+
+  attr_reader :state
+
+  def initialize
+    @state = STARTING_STATE
+  end
+
+  def die
+    @state = :dead
+  end
+
+  def live
+    @state = :alive
+  end
+end
---
+++ lib/grid.rb
@@ -0,0 +1,5 @@
+class Grid
+  def initialize(width, height)
+    
+  end
+end
