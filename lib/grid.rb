b7b15992e3506a5d5c295e89d8822f0bd29c65c9
carl <jerritt.aquino@gmail.com>
2020-04-02 22:09:11 -0400

    METHODS: find neighbors in grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -35,4 +35,24 @@ class Grid
 
     grid
   end
+
+  def alive_neighbors_count(i, j)
+    alive_neighbors(i, j).size
+  end
+
+  def alive_neighbors(i, j)
+    neighbors(i, j).select { |neighbor| neighbor.state == :alive }
+  end
+
+  def neighbors(i,j)
+    diff_idx = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
+
+    diff_idx.map do |di, dj|
+      if (i+di) < 0 || (i+di) >= @width || (j+dj) < 0 || (j+dj) >= @height
+        nil
+      else
+        @grid[(j+dj)][(i+di)]
+      end
+    end.select { |neighbor| neighbor }
+  end
 end
